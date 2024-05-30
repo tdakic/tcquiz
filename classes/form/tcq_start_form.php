83e35d94561fb71296081c8d7279946f125a13f6
Tamara Dakic <tdakic@gmail.com>
2024-05-30 09:57:01 -0700

    code compliance

--- classes/form/tcq_start_form.php
+++ classes/form/tcq_start_form.php
@@ -22,49 +22,64 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
+defined('MOODLE_INTERNAL') || die();
 require_once($CFG->libdir."/formslib.php");
 
+/**
+ * Form used by the teacher to start a tcquiz
+ *
+ * @package   quizaccess_tcquiz
+ * @copyright 2024 Tamara Dakic @Capilano University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class tcq_start_form extends moodleform {
 
+    /**
+     * Add fields to the form
+     */
     public function definition() {
 
         $mform = $this->_form;
 
-        //I want the two items in this form to be displayed side-by-side
-        //$mform->addElement('html', '<style> .fitem {display: inline-block;}</style>'); //moved to start_tcq.mustache
+        // I want the two items in this form to be displayed side-by-side.
 
         $mform->addElement('submit', 'start_new_session_button', get_string('startnewquiz', 'quizaccess_tcquiz'));
-        $mform->addElement('text', 'joincode', '',['placeholder'=> get_string('enterjoincode', 'quizaccess_tcquiz')]);
+        $mform->addElement('text', 'joincode', '', ['placeholder' => get_string('enterjoincode', 'quizaccess_tcquiz')]);
         $mform->addRule('joincode', get_string('err_alphanumeric', 'core_form'), 'regex', '/^[a-zA-Z0-9]+$/', 'client');
 
         $mform->setType('joincode', PARAM_TEXT);
 
-        //so the page can be redisplayed
+        // This is so the page can be redisplayed.
         $mform->addElement('hidden', 'id', intval($this->_customdata['cmid']));
         $mform->setType('id', PARAM_INT);
 
     }
 
-    // Custom validation of the joincode field
-    // Chose not to have the joincode as required, so checking if it is empty here
-    // If there is an existing session of the quiz, the joincode is not technically required
+    /** Custom validation of the joincode field
+     * Chose not to have the joincode as required, so checking if it is empty here.
+     * If there is an existing session of the quiz, the joincode is not technically required.
+     * @param $data submited data
+     * @param $files not used
+     */
     public function validation($data, $files) {
 
-      $errors = parent::validation($data,$files);
+        $errors = parent::validation($data, $files);
 
-      if ($data['joincode'] == ""){
-        $errors['joincode'] = '-'.get_string('err_required', 'core_form');
+        if ($data['joincode'] == "") {
+            $errors['joincode'] = '-'.get_string('err_required', 'core_form');
+            return $errors;
+        }
+
+        global $DB;
+        $tcquiz = $DB->get_record("quizaccess_tcquiz_session",
+              ['quizid' => $this->_customdata['quizid'], 'joincode' => $data['joincode']]);
+
+        if ($tcquiz) { // Teacher tried creating a new session, but a session with the same name exists.
+            $errors['joincode'] = '-'.get_string('sessionexisterror', 'quizaccess_tcquiz');
+            return $errors;
+        }
         return $errors;
-      }
-
-      global $DB;
-      $tcquiz = $DB->get_record("quizaccess_tcquiz_session", ['quizid' => $this->_customdata['quizid'],'joincode'=>$data['joincode']]);
-
-      if ($tcquiz){ //teacher tried creating a new session,but a session with the same name exists
-        $errors['joincode'] = '-'.get_string('sessionexisterror', 'quizaccess_tcquiz');
-        return $errors;
-      }
-      return $errors;
 
     }
 }
--- classes/form/tcq_student_join_form.php
+++ classes/form/tcq_student_join_form.php
@@ -22,56 +22,67 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
 require_once($CFG->libdir."/formslib.php");
 
+/**
+ * Form used by students to join a tcquiz
+ *
+ * @package   quizaccess_tcquiz
+ * @copyright 2024 Tamara Dakic @Capilano University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class tcq_student_join_form extends moodleform {
 
+    /**
+     * Add fields to the form
+     */
     public function definition() {
 
-        $mform = $this->_form;
+          $mform = $this->_form;
 
-        //If I want the two items in this form to be displayed side-by-side
-        //$mform->addElement('html', '<style> .fitem {display: inline-block;}</style>');
+          $mform->addElement('text', 'joincode', '', ['placeholder' => get_string('entercode', 'quizaccess_tcquiz'), 'size' => 25]);
+          $mform->addRule('joincode', null, 'required', null, 'client');
+          $mform->addRule('joincode', get_string('err_alphanumeric', 'core_form'), 'regex', '/^[a-zA-Z0-9]+$/', 'client');
 
-        $mform->addElement('text', 'joincode', '',['placeholder'=> get_string('entercode', 'quizaccess_tcquiz'), 'size'=>25]);
-        $mform->addRule('joincode', null, 'required', null, 'client');
-        $mform->addRule('joincode', get_string('err_alphanumeric', 'core_form'), 'regex', '/^[a-zA-Z0-9]+$/', 'client');
+          $mform->addElement('submit', 'join_session_button', get_string('jointcquiz', 'quizaccess_tcquiz'));
 
-        $mform->addElement('submit', 'join_session_button', get_string('jointcquiz', 'quizaccess_tcquiz'));
+          $mform->setType('joincode', PARAM_TEXT);
 
-        $mform->setType('joincode', PARAM_TEXT);
-
-        //so the page can be redisplayed
-        $mform->addElement('hidden', 'id', intval($this->_customdata['cmid']));
-        $mform->setType('id', PARAM_INT);
+          // This is so the page can be redisplayed.
+          $mform->addElement('hidden', 'id', intval($this->_customdata['cmid']));
+          $mform->setType('id', PARAM_INT);
 
     }
 
-    // Custom validation of the joincode field
-    // Chose not to have the joincode as required, so checking if it is empty here
-    // If there is an existing session of the quiz, the joincode is not technically required for teacher
-    public function validation($data,$files) {
+    /**
+     * Custom validation of the joincode field
+     * @param $data submited data
+     * @param $files not used
+     */
+    public function validation($data, $files) {
 
-      $errors = parent::validation($data,$files);
+        $errors = parent::validation($data, $files);
 
-      if ($data['joincode'] == ""){
-        $errors['joincode'] = '-'.get_string('err_required', 'core_form');
+        if ($data['joincode'] == "") {
+            $errors['joincode'] = '-'.get_string('err_required', 'core_form');
+            return $errors;
+        }
+
+        global $DB;
+        $tcquiz = $DB->get_record("quizaccess_tcquiz_session",
+          ['quizid' => $this->_customdata['quizid'], 'joincode' => $data['joincode']]);
+
+        if (!$tcquiz) { // No quiz with such joincode.
+            $errors['joincode'] = '-'.get_string('wrongjoincode', 'quizaccess_tcquiz');
+            return $errors;
+        }
+        if ($tcquiz->status == 0 || $tcquiz->status == 50) { // There is a quiz with such joincode, but it is not running.
+            $errors['joincode'] = '-'.get_string('quiznotrunning', 'quizaccess_tcquiz');
+            return $errors;
+        }
+
         return $errors;
-      }
-
-      global $DB;
-      $tcquiz = $DB->get_record("quizaccess_tcquiz_session", ['quizid' => $this->_customdata['quizid'],'joincode'=>$data['joincode']]);
-
-      if (!$tcquiz){ //no quiz with such joincode
-        $errors['joincode'] = '-'.get_string('wrongjoincode', 'quizaccess_tcquiz');
-        return $errors;
-      }
-      if ($tcquiz->status == 0 || $tcquiz->status == 50) { //there is a quiz with such joincode, but it is not running
-        $errors['joincode'] = '-'.get_string('quiznotrunning', 'quizaccess_tcquiz');
-        return $errors;
-      }
-
-      return $errors;
 
     }
 }
